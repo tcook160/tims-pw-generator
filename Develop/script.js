4e308b9f899c37c086dca40b3637a05517416f5d
tcook160 <112792888+tcook160@users.noreply.github.com>
2022-10-08 10:07:34 -0700

    Updating script.js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+
+//declarations
 var confirmLength;
 var confirmLowerCase;
 var confirmUpperCase;
@@ -6,7 +8,7 @@ var confirmSpecial;
 var confirmNumber;
 var userSelection;
 
-
+//variables for password criteria
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
